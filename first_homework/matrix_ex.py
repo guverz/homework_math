589653a5b70c3747a153a95a5af7dfdfd9874f8e
guverz <guverz1994@mail.ru>
2022-12-29 01:32:28 +0800

    Функции по матрицам работают

--- first_homework/matrix_ex.py
+++ first_homework/matrix_ex.py
@@ -1,3 +1,4 @@
+import math_ex as me
 def empty_matrix(a): #Создание пустой матрицы 
     end_f = []
     for _ in range(len(a)):
@@ -12,11 +13,97 @@ def sum_matrix(a, b): #Сложение матриц
     return end
 
 def sub_matrix(a, b): #Вычитание матриц
-    end = []
-    for _ in range(len(a)):
-        end.append([])
+    end = empty_matrix(a)
     for j in range(len(a[0])):
         for i in range(len(b)):
             end[i].append(a[i][j] - b[i][j])
     return end
 
+def matrix_trans(a): #Транспонирование матриц
+    return [
+        list(end)
+        for end in zip(*a)
+    ]
+
+def mult_matrix_skal(a, s): #Умножение матриц на скаляр
+    end = empty_matrix(a)
+    for i in range(len(a)):
+        #me.mult_scal_vector(a[i], s)
+        for j in range(len((a)[i])):
+            end[i].append(a[i][j] * s)
+    return end
+
+def get_row_i(a, n): #Получение строки по индексу
+    return a[n]
+
+def get_col_i(a, n): #Получение столбца по индексу
+    return get_row_i(matrix_trans(a), n)
+
+def row_replacement(a, n, n2): #Перестановка строк n - индекс 1-й строки для замены, n2 - индекс 2-й строки для замены
+    end = empty_matrix(a)
+    for k in range(len(get_row_i(a, n))):
+        end[n2].append((get_row_i(a, n)[k]))
+        end[n].append((get_row_i(a, n2)[k]))   
+    for i in range(len(a)):
+        if i != n2 and i != n:
+            for j in range(len(a[i])):
+                end[i].append((get_row_i(a, i)[j]))
+    return end
+
+
+def row_skal_mult(a, n, s): #Умножение строки матрицы на скаляр
+    end = empty_matrix(a)
+    for k in range(len(get_row_i(a, n))):
+        end[n].append((me.mult_scal_vector(get_row_i(a, n), s)[k]))
+    for i in range(len(a)):
+        if i != n:
+            for j in range(len(a[i])):
+                end[i].append((get_row_i(a, i)[j]))
+    return end
+
+#def row_inserting(a, end, n):
+#    for k in range(len(a[n])):
+#        end[n].append((a[n][k]))
+#    return end
+
+#def wondering(a):
+#    for n in range(len(a)):
+#        end = row_inserting(a, end1, n)
+#    return end
+
+#a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+#end1 = empty_matrix(a)
+#print(wondering)
+
+def mult_of_sum_matrix(a, s, n, n1): #Сложение строк, умноженных на число (n - строка из которой вычитаем строку n1 * s)    
+    end = empty_matrix(a)
+    for k in range(len(get_row_i(a, n))):
+        end[n].append((me.sum_vector(get_row_i(a, n), me.mult_scal_vector(get_row_i(a, n1), s))[k]))
+    for i in range(len(a)):
+        if i != n:
+            for j in range(len(a[i])):
+                end[i].append((get_row_i(a, i)[j]))
+    return end
+
+def mult_of_sub_matrix(a, s, n, n1): #Bычитание строк, умноженных на число (n - строка из которой вычитаем строку n1 * s)    
+    end = empty_matrix(a)
+    for k in range(len(get_row_i(a, n))):
+        end[n].append((me.sub_vector(get_row_i(a, n), me.mult_scal_vector(get_row_i(a, n1), s))[k]))
+    for i in range(len(a)):
+        if i != n:
+            for j in range(len(a[i])):
+                end[i].append((get_row_i(a, i)[j]))
+    return end
+
+def empty_matrix_for_mult(a, b): #Создание пустой матрицы для умножения матриц
+    m = len(a)                                                                                        
+    k = len(b[0])
+    return [[None for __ in range(k)] for __ in range(m)] 
+
+def matrix_mult(a, b): #Умножение матриц
+    end = empty_matrix_for_mult(a, b)
+    print(end)
+    for i in range(len(a)):
+        for j in range(len(b[0])):       
+            end[i][j] = sum(a[i][m] * b[m][j] for m in range(len(b)))
+    return end
--- first_homework/test_matrix_ex.py
+++ first_homework/test_matrix_ex.py
@@ -1,14 +1,90 @@
 import matrix_ex as mxe
 def test_sum_matrix():
-    a = [[1, 2, 3],[3, 2, 1]]
-    b = [[3, 2, 1],[1, 2, 5]]
+    a = [[1, 2, 3], [3, 2, 1]]
+    b = [[3, 2, 1], [1, 2, 5]]
     exp = [[4, 4, 4], [4, 4, 6]]
     res = mxe.sum_matrix(a,b)
     assert res == exp
 
 def test_sub_matrix():
-    a = [[1, 2, 3],[3, 2, 1]]
-    b = [[3, 2, 1],[1, 2, 5]]
+    a = [[1, 2, 3], [3, 2, 1]]
+    b = [[3, 2, 1], [1, 2, 5]]
     exp = [[-2, 0, 2], [2, 0, -4]]
     res = mxe.sub_matrix(a,b)
+    assert res == exp
+
+def test_matrix_trans():
+    a = [[1, 2, 3], [3, 4, 5]]
+    exp = [[1, 3], [2, 4], [3, 5]]
+    res = mxe.matrix_trans(a)
+    assert res == exp
+
+def test_mult_matrix_skal():
+    a = [[1, 2, 3], [3, 4, 5]]
+    s = 2
+    exp = [[2, 4, 6], [6, 8, 10]]
+    res = mxe.mult_matrix_skal(a, s)
+    assert res == exp
+
+def test_get_row_i():
+    a = [[1, 2, 3], [3, 4, 5]]
+    n = 1
+    exp = [3, 4, 5]
+    res = mxe.get_row_i(a, n)
+    assert res == exp
+
+def test_get_col_i():
+    a = [[1, 2, 3], [3, 4, 5]]
+    n = 1
+    exp = [2, 4]
+    res = mxe.get_col_i(a, n)
+    assert res == exp
+
+def test_row_replacement():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    n = 0
+    n2 = 2
+    exp = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [10, 11, 12]]
+    res = mxe.row_replacement(a, n, n2)
+    assert res == exp
+
+def test_row_skal_mult():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    n = 0
+    s = 4
+    exp = [[4, 8, 12], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    res = mxe.row_skal_mult(a, n, s)
+    assert res == exp
+
+def test_row_inserting():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    n = 0
+    end = mxe.empty_matrix(a)
+    exp = [[1, 2, 3], [], [], []]
+    res = mxe.row_inserting(a, end, n)
+    assert res == exp
+
+def test_mult_of_sum_matrix():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    n1 = 0
+    n = 2
+    s = 2
+    exp = [[1, 2, 3], [4, 5, 6], [9, 12, 15], [10, 11, 12]]
+    res = mxe.mult_of_sum_matrix(a, s, n, n1)
+    assert res == exp
+
+def test_mult_of_sub_matrix():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    n1 = 0
+    n = 2
+    s = 2
+    exp = [[1, 2, 3], [4, 5, 6], [5, 4, 3], [10, 11, 12]]
+    res = mxe.mult_of_sub_matrix(a, s, n, n1)
+    assert res == exp
+
+def test_matrix_mult():
+    a = [[1], [2], [3]]
+    b = [[2, 4]]
+    exp = [[2, 4], [4, 8], [6, 12]]
+    res = mxe.matrix_mult(a, b)
     assert res == exp
